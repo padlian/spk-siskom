0f32fe6a504a18d864024a77d292e940d9a1abaa
Padlian Chairi <padlian25@gmail.com>
2019-03-27 21:07:37 +0700

    penambahan list data alternatif

--- tugas2/index.php
+++ tugas2/index.php
@@ -14,6 +14,43 @@ $result = mysqli_query($mysqli, "SELECT * FROM tbl_alternatif_wp ORDER BY id ASC
 	<title>SPK Metode WP</title>
 </head>
 <body>
+<a href="tambah.php">Tambah</a>
+
+Bobot Nilai = <?= $b_nilai ?> <br./>
+Bobot Kehadiran = <?= $b_kehadiran ?> <br./>
+Bobot Penghasilan Orang Tua = <?= $b_penghasilan_ortu ?> <br./>
+Bobot Jumlah Tanggungan Orang Tua = <?= $b_tanggungan_ortu ?> <br./>
+
+<h1>Data setiap alternatif</h1>
+<table border=1>
+	<tr>
+		<th>nama</th>
+		<th>nilai</th>
+		<th>kehadiran</th>
+		<th>penghasilan_ortu</th>
+		<th>tanggungan_ortu</th>
+	</tr>
+	<?php
+	$persamaan2 = mysqli_query($mysqli, "SELECT * FROM tbl_alternatif_wp ORDER BY id ASC");
+	$total = 0;
+	while ($user_data = mysqli_fetch_array($persamaan2)) {
+		$hasil = pow($user_data['nilai'], $b_nilai) * 
+				pow($user_data['kehadiran'], $b_kehadiran) * 
+				pow($user_data['penghasilan_ortu'], -$b_penghasilan_ortu) *
+				pow($user_data['tanggungan_ortu'], -$b_tanggungan_ortu);
+		$total+=$hasil;
+		?>
+			<tr>
+				<td><?= $user_data['nama'] ?></td>
+				<td><?= $user_data['nilai'] ?></td>
+				<td><?= $user_data['kehadiran'] ?></td>
+				<td><?= $user_data['penghasilan_ortu'] ?></td>
+				<td><?= $user_data['tanggungan_ortu'] ?></td>
+			</tr>
+		<?php
+	}
+	?>
+</table>
 	
 <h1>Persamaan 2</h1>
 <table border=1>
@@ -40,9 +77,6 @@ $result = mysqli_query($mysqli, "SELECT * FROM tbl_alternatif_wp ORDER BY id ASC
 		<?php
 	}
 	?>
-	<tr>
-		<td></td>
-	</tr>
 </table>
 
 
@@ -65,9 +99,6 @@ $result = mysqli_query($mysqli, "SELECT * FROM tbl_alternatif_wp ORDER BY id ASC
 		<?php
 	}
 	?>
-	<tr>
-		<td></td>
-	</tr>
 </table>
 
 </body>
